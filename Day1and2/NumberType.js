7c64ef94c8dc9da37dd79304cfa466c14e20dec8
NirmalRaj2023 <129010781+NirmalRaj2023@users.noreply.github.com>
2026-03-04 16:48:35 +0530

    Add files via upload
    Day1 and Day 2 assignments

---
+++ Day1and2/NumberType.js
@@ -0,0 +1,11 @@
+function num()
+{
+    let num = 1
+    if(num>0)
+    console.log("number is positive")   
+    else if(num<0)
+    console.log("number is negative")
+    else
+    console.log("zero zero")
+}
+num()
